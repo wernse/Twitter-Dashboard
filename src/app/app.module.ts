116aa51fd2f9d08f3db9f02601c3712d1ccbf7d8
Unknown <magic_Cards9@hotmail.com>
2018-03-28 08:12:38 +1300

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatCardModule } from "@angular/material/card";
 
 import { AppComponent } from "./app.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
@@ -10,7 +12,12 @@ import { DataService } from "../providers/data/data.service";
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent, NavbarComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
